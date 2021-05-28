1bcd11e07cfca05fcafc8d455da05953d1bf7a88
Bsupinski <66806979+bsupinski@users.noreply.github.com>
2021-05-28 18:36:15 -0400

    added Enter key up event to input

--- js/message.js
+++ js/message.js
@@ -3,7 +3,7 @@ const sendMessage = document.getElementById("send");
 const inputValue = document.getElementById("message-input");
 
 
-
+//Send input if Sned key entered.
 sendMessage.addEventListener("click", e =>{
     if(inputValue.value == ""){
         return false;
@@ -11,6 +11,17 @@ sendMessage.addEventListener("click", e =>{
         chatBox.innerHTML += `<div class="new-message"><span> Current User:</span>${inputValue.value}</div>`;
         inputValue.value="";
     }
+});
 
-   
+//Send input if enter is hit on keyboard
+
+inputValue.addEventListener("keyup", (e)=> {
+    if ( e.keycode === 13) {
+        if(inputValue.value == ""){
+            return false;
+        }else{
+            chatBox.innerHTML += `<div class="new-message"><span> Current User:</span>${inputValue.value}</div>`;
+            inputValue.value="";
+        }
+    }
 })
