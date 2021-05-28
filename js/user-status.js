02cf029320366e8d41e653959b3edb16c15a8fa7
Bsupinski <66806979+bsupinski@users.noreply.github.com>
2021-05-28 17:44:23 -0400

    working on status

--- js/user-status.js
+++ js/user-status.js
@@ -1,15 +1,37 @@
-const statusSelect = document.getElementById("standard-select").value;
+const statusSelect = document.getElementById("standard-select");
 const statusColor = document.getElementById("user-status");
 
-console.log(statusSelect)
-function statusCheck(){
-    if (statusSelect === "online") {
+// console.log(statusSelect)
+// function statusCheck(){
+//     if (statusSelect === "online") {
+//         statusColor.style.backgroundColor = "#94f6d3";
+//     }else if (statusSelect === "away") {
+//         statusColor.style.backgroundColor = "#fcd7a1";
+//     } else{
+//         statusColor.style.backgroundColor = "#cfd8d4";
+//     }
+// }
+// statusCheck()
+// setInterval(statusCheck(), 1000)
+
+
+
+statusSelect.addEventListener("click",() => {
+    if (statusSelect.value === "online") {
         statusColor.style.backgroundColor = "#94f6d3";
-    }else if (statusSelect === "away") {
+    }else if (statusSelect.value === "away") {
         statusColor.style.backgroundColor = "#fcd7a1";
     } else{
         statusColor.style.backgroundColor = "#cfd8d4";
     }
-}
-statusCheck()
-setInterval(statusCheck(), 1000)
+})
+statusSelect.addEventListener("load",() => {
+    if (statusSelect.value === "online") {
+        statusColor.style.backgroundColor = "#94f6d3";
+    }else if (statusSelect.value === "away") {
+        statusColor.style.backgroundColor = "#fcd7a1";
+    } else{
+        statusColor.style.backgroundColor = "#cfd8d4";
+    }
+})
+
